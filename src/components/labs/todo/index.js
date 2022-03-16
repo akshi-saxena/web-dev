b3b15d0869dcf1ab0cb41f95709b32312c6d23cd
Akshi Saxena <akshi305.as@gmail.com>
2022-03-15 23:43:16 -0400

    a6 React.js labs

---
+++ src/components/labs/todo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./todo-list";
+
+const Todo = () => {
+    return(
+        <>
+            <h1>Todo List</h1>
+            <TodoList/>
+        </>
+    )
+};
+export default Todo;
+

7fd7294029a97725341081ef42ecf1d192fd197b
Akshi Saxena <akshi305.as@gmail.com>
2022-04-10 01:32:38 -0400

    a9 node

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const TUITS_API = 'http://localhost:4000/api/tuits';
 // const TUITS_API = 'https://web-development-server.herokuapp.com/api/tuits';
+// const TUITS_API = 'http://localhost:4000/api/tuits';
 
 const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
